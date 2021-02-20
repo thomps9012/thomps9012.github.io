c99d55d4aec076c08d9c55e562b2ec8338c5220c
Samuel Thompson <thomps9012@gmail.com>
2021-02-20 09:20:23 -0500

    restoring previous working version

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/NaveenDA/naveenda.github.io/gh-pages/' : '',
+    assetPrefix: isProd ? 'https://cdn.statically.io/gh/thomps9012/thomps9012.github.io/gh-pages/' : '',
 }
