b7b16a17b97e23683933cc42d72d348e4db3703b
Samuel Thompson <thomps9012@gmail.com>
2021-02-20 08:57:15 -0500

    updating files

---
+++ env-config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack')
+
+const isProd = (process.env.NODE_ENV || 'production') === 'production'
+
+const assetPrefix = isProd ? '/NextJSBlog' : ''
+
+module.exports = {
+  exportPathMap: () => ({
+    '/': { page: '/' },
+    '/page1': { page: '/page1' },
+  }),
+  assetPrefix: assetPrefix,
+  webpack: config => {
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        'process.env.ASSET_PREFIX': JSON.stringify(assetPrefix),
+      }),
+    )
+
+    return config
+  },
+}
--- next.config.js
+++
@@ -1,5 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-    assetPrefix: isProd ? 'https://cdn.statically.io/gh/thomps9012/thomps9012.github.io/gh-pages/' : '',
-}
