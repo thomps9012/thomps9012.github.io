29d876ae5fff61fb1b0018b7d0f8a801a059de88
Samuel Thompson <thomps9012@gmail.com>
2021-02-21 11:26:39 -0500

    removed statically cdn

--- next.config.js
+++
@@ -1,6 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production'
-
-module.exports = {
-  // Use the CDN in production and localhost for development.
-  assetPrefix: isProd ? 'https://cdn.statically.io/gh/thomps9012/thomps9012.github.io/gh-pages/' : '',
-}
