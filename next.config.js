2ab94f428915320157c424f726bf6cd9e7d6e0b1
Samuel Thompson <thomps9012@gmail.com>
2021-02-21 09:13:31 -0500

    added in an env and next config files

---
+++ next.config.js
@@ -0,0 +1,10 @@
+  
+// This file is not going through babel transformation.
+// So, we write it in vanilla JS
+// (But you could use ES2015 features supported by your Node.js version)
+
+const debug = process.env.NODE_ENV !== 'production'
+
+module.exports = {
+  assetPrefix: !debug ? '/thomps.9012.github.io/' : '',
+}
