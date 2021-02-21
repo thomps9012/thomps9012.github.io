02bcb356bc195afd4ee7de0f95b32cfb92a3f893
Samuel Thompson <thomps9012@gmail.com>
2021-02-21 10:29:20 -0500

    reconfigured next.config.js

--- next.config.js
+++ next.config.js
@@ -1,35 +1,6 @@
-// This file is not going through babel transformation.
-// So, we write it in vanilla JS
-// (But you could use ES2015 features supported by your Node.js version)
-
-const debug = process.env.NODE_ENV !== "production";
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" }
-    }
-  },
-  //assetPrefix: '',
-  assetPrefix: !debug ? '/thomps9012.github.io/' : '',
-  webpack: (config, { dev }) => {
-    // Perform customizations to webpack config
-    // console.log('webpack');
-    // console.log(config.module.rules, dev);
-    config.module.rules = config.module.rules.map(rule => {
-      if(rule.loader === 'babel-loader') {
-        rule.options.cacheDirectory = false
-      }
-      return rule
-    })
-    // Important: return the modified config
-    return config
-  }/*,
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
-    // console.log('webpackDevMiddleware');
-    // console.log(config);
-    // Important: return the modified config
-    return config
-  }, */
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'https://cdn.statically.io/gh/thomps9012/thomps9012.github.io/gh-pages/' : '',
 }
